a0adfd2cbe6b7deacf82686a510af59873502270
smilykuy <smilykuy8@gmail.com>
2021-08-21 10:50:06 +0700

    build project

---
+++ src/main/java/smily/plugin/randomevent/RandomEvent.java
@@ -0,0 +1,17 @@
+package smily.plugin.randomevent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RandomEvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
